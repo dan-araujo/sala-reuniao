ff8c8ea97716820491d91a726353db53e52157f8
Daniel Araújo <daniel.ti.info@gmail.com>
2021-09-10 18:06:23 -0300

    Initial commit

---
+++ src/main/java/br/com/santander/fullstack/developer/sala/reuniao/SalaReuniaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.santander.fullstack.developer.sala.reuniao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaReuniaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaReuniaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/santander/fullstack/developer/sala/reuniao/SalaReuniaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.santander.fullstack.developer.sala.reuniao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaReuniaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
